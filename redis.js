f02d91d5a613de51f71655780d648ce93f872fa6
GeniusPRO271 <benjamintoroj@gmail.com>
2023-05-03 11:00:47 +0700

    fix redis

--- redis.js
+++ redis.js
@@ -1,11 +1,9 @@
 const redis = require('redis');
 require('dotenv').config();
 const client = redis.createClient({
-  host: process.env.REDIS_HOST,
-  port: process.env.REDIS_PORT,
-  password: process.env.REDIS_PASSWORD,
+  legacyMode: true,
+  url: process.env.REDIS_URI,
 });
-
 client.on('connect', () => {
   console.log('Redis client connected');
 });
